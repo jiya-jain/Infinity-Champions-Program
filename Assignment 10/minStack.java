27968b8e27da46d36e909b751e7ada427ef022c3
Jiya Jain <jiya.jain479@gmail.com>
2025-10-24 19:30:15 +0530

    Implement MinStack with push, pop, top, and getMin

---
+++ Assignment 10/minStack.java
@@ -0,0 +1,42 @@
+class MinStack {
+    public Stack<Integer> stack = new Stack<>();
+    public Stack<Integer> minStack = new Stack<>();
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int val) {
+        stack.push(val);
+
+        if(minStack.isEmpty() || val<=minStack.peek()){
+            minStack.push(val);
+        }
+    }
+
+    public void pop() {
+        int remove = stack.pop();
+
+        if(minStack.peek()==remove){
+            minStack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+       return minStack.peek();
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
